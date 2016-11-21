bcfe3c9b3ca6f3e1466ae10f914cc64ad0432ded
Daniel Jackson <d95jackson@gmail.com>
2016-11-21 18:50:22 +0000

    Added a nodesj server

---
+++ Web/server.js
@@ -0,0 +1,21 @@
+/**
+ * Application configuration variables
+ */
+// Node server port
+var PORT = 3000;
+
+// Requirements for starting the node server
+var express = require('express');
+var app     = express();
+var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
+
+
+// All front-end files should be placed in the public directory
+app.use(express.static(__dirname));
+
+
+console.log("Launching server from: "+__dirname);
+http.listen(PORT, function(){
+    console.log('listening on *:'+PORT);
+});
