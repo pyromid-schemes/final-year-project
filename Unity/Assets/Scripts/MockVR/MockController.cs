ada40e9c78c1b2b31db4447cca4b8b36717f67fe
DanC <kcc25@kent.ac.uk>
2016-11-24 18:23:47 +0000

    Implemented MockVr player that can move

---
+++ Unity/Assets/Scripts/MockVR/MockController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MockController : MonoBehaviour {
+   
+    private CharacterController controller;
+    private float speed = 3F;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+       
+        UpdatePositionOfCharacterIgnoringCollisions(horizontal,vertical);
+        UpdateCharactersFowardPositionForCollisions(vertical);
+    }
+
+    void UpdatePositionOfCharacterIgnoringCollisions(float horizontal, float vertical)
+    {
+        Vector3 newPosition = new Vector3(horizontal *= Time.deltaTime, 0, vertical *= Time.deltaTime);
+        transform.Translate(newPosition);
+    }
+
+    void UpdateCharactersFowardPositionForCollisions(float vertical)
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        controller.SimpleMove(forward * vertical);
+    }
+}
