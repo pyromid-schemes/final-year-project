357ca4e96271189c7352f2f50e4c4fc8e45076e8
DanC <kcc25@kent.ac.uk>
2016-11-28 16:11:17 +0000

    MockVr player, now a knight, can now attack on mouse click

--- Unity/Assets/Scripts/MockVR/MockController.cs
+++ Unity/Assets/Scripts/MockVR/MockController.cs
@@ -1,23 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MockController : MonoBehaviour {
-   
+public class MockController : MonoBehaviour
+{
+
     private CharacterController controller;
     private float speed = 3f;
+    private const float MeleeMaxRange = 3f;
+    private Animator animator;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * speed;
         float vertical = Input.GetAxis("Vertical") * speed;
-       
-        UpdatePositionOfCharacterIgnoringCollisions(horizontal,vertical);
+
+        UpdatePositionOfCharacterIgnoringCollisions(horizontal, vertical);
         UpdateCharactersFowardPositionForCollisions(vertical);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("left mouse down for fire");
+            animator.Play("Attack");
+        }
+
     }
 
     private void UpdatePositionOfCharacterIgnoringCollisions(float horizontal, float vertical)
