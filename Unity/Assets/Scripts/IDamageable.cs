a3ca931f3435c24779b936551f118af27d16b27e
DanC <kcc25@kent.ac.uk>
2016-12-02 10:58:01 +0000

    Started combat, work in progress

---
+++ Unity/Assets/Scripts/IDamageable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IDamageable
+{
+    //Checks if the collision comes from a weapon
+    bool CheckCollisionIsFromADamageSource(string damageSource);
+
+    //Gets the damage from the weapon and applies to the player
+    void ApplyDamage(int damage);
+}
+
---
+++ Unity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,15 @@
+﻿public abstract class Weapon{
+    private int damage;
+
+    public Weapon(int damage)
+    {
+        this.damage = damage;
+    }
+
+    public int GetTotalDamage()
+    {
+        return CalculateDamage();
+    }
+
+    protected abstract int CalculateDamage();
+}
