d3199fb47a93e26393c67da346da361fdcb9bb06
DanC <kcc25@kent.ac.uk>
2016-12-05 13:16:57 +0000

    Implemented the ability to attack a mob, mob now takes damage to health

---
+++ Unity/Assets/Scripts/CombatSystem/Lance.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Lance : Weapon {
+
+    public Lance(): base(1)
+    {
+    }
+}
---
+++ Unity/Assets/Scripts/CombatSystem/TestHit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TestHit : Damageable {
+   
+
+    public TestHit() : base(2)
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (isColliding()) return;
+        SetColliding(true);
+
+        if (CheckCollisionIsFromADamageSource(other.gameObject.tag))
+        {
+            Debug.Log("Mob: Im hit");
+            int damage = other.GetComponent<Weapon>().GetDamage();
+            ApplyDamage(damage);
+        }
+        if(CheckHealthIsZero())
+        {
+            OnDeath();
+        }
+    }
+
+    protected override void OnDeath()
+    {
+        Debug.Log(this.gameObject.ToString());     
+        GameObject.Destroy(this.gameObject);
+    }
+}
