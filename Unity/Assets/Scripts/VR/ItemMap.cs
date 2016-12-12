dbcaf82b3a1ded13c17d95f44e8583621d904aa4
Japeth <Japeth.Gurr@hotmail.co.uk>
2016-12-12 15:43:42 +0000

    Added basics of item equipping, and restructured assets slightly

---
+++ Unity/Assets/Scripts/VR/ItemMap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMap : MonoBehaviour {
+    public GameObject swordPrefab;
+
+    public Transform GetDefaultTransform(string itemName)
+    {
+        switch (itemName)
+        {
+            case "Block Sword":
+                return swordPrefab.transform;
+            default:
+                throw new System.Exception("ItemMap.GetDefaultTransform() could not find the requested prefab");
+        }
+    }
+}
