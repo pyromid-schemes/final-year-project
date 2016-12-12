0145c856f3cbe86aa5395c98a82a52777e17759c
Japeth <Japeth.Gurr@hotmail.co.uk>
2016-12-12 16:29:35 +0000

    Updated prefabs and added Lance

--- Unity/Assets/Scripts/VR/ItemMap.cs
+++ Unity/Assets/Scripts/VR/ItemMap.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class ItemMap : MonoBehaviour {
     public GameObject swordPrefab;
+    public GameObject lancePrefab;
 
     public Transform GetDefaultTransform(string itemName)
     {
         switch (itemName)
         {
-            case "Block Sword":
+            case "PlayerSword":
                 return swordPrefab.transform;
+            case "PlayerLance":
+                return lancePrefab.transform;
             default:
                 throw new System.Exception("ItemMap.GetDefaultTransform() could not find the requested prefab");
         }
---
+++ Unity/Assets/Scripts/Weapon/Sword.cs
@@ -0,0 +1,9 @@
+﻿public class Sword : Weapon
+{
+    private const int baseDamage = 1;
+
+    public Sword() : base(baseDamage)
+    {
+    }
+
+}
