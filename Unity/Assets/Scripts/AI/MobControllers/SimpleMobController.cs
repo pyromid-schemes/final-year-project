1ae47072216dc0c1a7b36693765950ee93b8bc35
Daniel Burnley <dan@danops.uk>
2017-02-05 13:39:35 +0000

    Refactor SimpleSkeletonController a little

--- Unity/Assets/Scripts/AI/MobControllers/SimpleMobController.cs
+++ Unity/Assets/Scripts/AI/MobControllers/SimpleMobController.cs
@@ -1,12 +1,29 @@
-﻿using AI.States;
+﻿using AI.Pathfinding;
+using AI.States;
 using UnityEngine;
 
-namespace AI.MobControllers 
+namespace AI.MobControllers
 {
     abstract class SimpleMobController : MonoBehaviour
     {
         protected StateManager StateManager;
         protected Animator Anim;
+        [HideInInspector] public Grid Grid;
+
+        public Transform Eyes;
+        public float AttackCooldown = 2f;
+        public float AttackRange = 2f;
+        public float RotateSpeed = 10f;
+        public float FOV = 180f;
+
+        protected abstract void InitialiseStates();
+
+        void Start()
+        {
+            Anim = GetComponent<Animator>();
+            Grid = GameObject.Find("Grid").GetComponent<Grid>();
+            InitialiseStates();
+        }
 
         // Update is called once per frame
         void Update()
