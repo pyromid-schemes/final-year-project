81364db858a5cf196f9371c50634bc89c27db20d
Jamie Redding <jamieredding2694@gmail.com>
2017-02-15 11:03:23 +0000

    Removing Unity-unfriendly tests.

--- Unity/Assets/Scripts/Test/Editor/Web/JsonMessageBuilderTest.cs
+++ Unity/Assets/Scripts/Test/Editor/Web/JsonMessageBuilderTest.cs
@@ -9,10 +9,6 @@ namespace Test.Web
 	public class JsonMessageBuilderTest
 	{
 
-		public JsonMessageBuilderTest ()
-		{
-		}
-
 		[Test]
 		public void FormatVRPositionIntoJsonVRPositionMessage ()
 		{
@@ -23,34 +19,5 @@ namespace Test.Web
 
 			Assert.AreEqual (expected, JsonMessageBuilder.FormatVRPosition (new Vector3 (10, 0, -20)));
 		}
-
-//		[Test]
-//		public void FormatRoomIntoWorldStatusMessage ()
-//		{
-//			string expected = "{" +
-//			                  "\"objectId\":\"room1\"," +
-//			                  "\"xPos\":10," +
-//			                  "\"zPos\":20," +
-//			                  "\"rot\":0" +
-//			                  "}";
-//
-//			Assert.AreEqual (expected, JsonMessageBuilder.FormatRoom (new PlacedPrefab ("room1", new Vector3 (10, 0, -20), Quaternion.identity)));
-//		}
-
-//		[Test]
-//		public void FormatSingleMobAsJsonMessage ()
-//		{
-//			string expected = "{" +
-//				"\"objectId\":\"mobKnight\"," +
-//				"\"xPos\":10," +
-//				"\"zPos\":20," +
-//				"\"id\":0," +
-//				"\"dead\":false" +
-//				"}";
-//			GameObject mob = new GameObject ("test");
-//			mob.transform.Translate (10, 0, -20);
-//
-//			Assert.AreEqual (expected, JsonMessageBuilder.FormatMob (new PlacedMob ("mobKnight", 0, mob)));
-//		}
 	}
 }
