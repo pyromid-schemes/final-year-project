15171160bd6b69f29bf18ed3525c2e41ef8c6e51
Japeth <Japeth.Gurr@hotmail.co.uk>
2017-02-15 15:57:49 +0000

    Updated player prefab and added MovementMethod enum

--- Unity/Assets/Scripts/VR/Movement.cs
+++ Unity/Assets/Scripts/VR/Movement.cs
@@ -7,6 +7,15 @@ namespace VirtualReality
     [RequireComponent(typeof(SteamVR_TrackedObject))]
     public class Movement : MonoBehaviour
     {
+        public enum MovementType
+        {
+            HeadTracked,
+            HandTracked,
+            TouchJoystick,
+            PressJoystick,
+            Teleportation,
+            GrabAndThrow
+        }
 
         SteamVR_TrackedObject TrackedOBJ;
         SteamVR_Controller.Device Device;
@@ -14,27 +23,37 @@ namespace VirtualReality
         public GameObject Player;
         public GameObject PlayerHead;
 
+        public MovementType ChosenMovement;
+
         GameObject Hand;
 
-		public IMovementMethod movementMethod;
+        IMovementMethod MovementMethod;
 
         // Get this object
         void Awake()
         {
             TrackedOBJ = GetComponent<SteamVR_TrackedObject>();
             Hand = this.gameObject;
+            switch (ChosenMovement)
+            {
+                case MovementType.HeadTracked: MovementMethod = new RelativeToPlayerHead(); break;
+                case MovementType.HandTracked: MovementMethod = new RelativeToHand(); break;
+                case MovementType.TouchJoystick: MovementMethod = new TouchTouchpad(); break;
+                case MovementType.PressJoystick: MovementMethod = new PressTouchpad(); break;
+                case MovementType.GrabAndThrow: MovementMethod = new GrabAndThrow(); break;
+            }
         }
 
         void FixedUpdate()
         {
             Device = SteamVR_Controller.Input((int)TrackedOBJ.index);
 
-			if (movementMethod.BeginMovement (Device)) {
-				Vector3 position = movementMethod.RunMovement (PlayerHead, Hand, Player, Device);
+			if (MovementMethod.BeginMovement (Device)) {
+				Vector3 position = MovementMethod.RunMovement (PlayerHead, Hand, Player, Device);
 				Player.transform.Translate (position);
 			} else {
 				Vector3 position;
-				if (movementMethod.IdleMovement (Device, out position)) {
+				if (MovementMethod.IdleMovement (Device, out position)) {
 					Player.transform.Translate (position);
 				}
 			}
