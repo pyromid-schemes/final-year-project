204c244830aa5d06f2038285da07f4b7f6b31a54
Jamie Redding <jamieredding2694@gmail.com>
2017-02-20 19:52:47 +0000

    Creating IPCManager with tests

---
+++ Unity/Assets/Scripts/Communication/Event.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Communication
+{
+	public interface Event
+	{
+		EventType GetEventType ();
+	}
+}
+
---
+++ Unity/Assets/Scripts/Communication/IPCManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Communication
+{
+	public class IPCManager
+	{
+		private List<Event> events;
+
+		public IPCManager ()
+		{
+			events = new List<Event> ();
+		}
+
+		public List<Event> ReceiveEventsForType(EventType type)
+		{
+			var ret = events.FindAll (e => e.GetEventType ().Equals (type));
+			events.RemoveAll (e => e.GetEventType ().Equals (type));
+			return ret;
+		}
+
+		public void RegisterEvent (Event e)
+		{
+			events.Add (e);
+		}
+	}
+}
+
