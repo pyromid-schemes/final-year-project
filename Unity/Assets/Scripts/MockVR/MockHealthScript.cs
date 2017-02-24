3ddbb816eae3461c2aad092976c7b0b419eeb97b
DanC <kcc25@kent.ac.uk>
2017-02-24 11:14:08 +0000

    Updated mock player to have a health script and box collider

---
+++ Unity/Assets/Scripts/MockVR/MockHealthScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MockHealthScript : MonoBehaviour, IDamageable {
+    private int maxHealth;
+    private int health;
+
+    private GameObject self;
+    private bool isDead;
+
+    public MockHealthScript()
+    {
+        maxHealth = 10;
+        health = maxHealth;
+        isDead = false;
+    }
+
+    void Start()
+    {
+        self = this.gameObject;
+    }
+
+    void OnCollisionEnter()
+    {
+        if (HealthIsZero())
+        {
+            OnZeroHealth();
+        }
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public bool HealthIsZero()
+    {
+        return health <= 0;
+    }
+
+    public void OnZeroHealth()
+    {
+        self.SetActive(false);
+        isDead = true;
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+}
