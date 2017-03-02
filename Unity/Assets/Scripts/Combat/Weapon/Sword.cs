ac56d7636c29593f34d0db42ce85176da3a04c37
DanC <kcc25@kent.ac.uk>
2017-03-02 13:29:17 +0000

    WIP added damage range, chance for crit and crit multipler

--- Unity/Assets/Scripts/Combat/Weapon/Sword.cs
+++ Unity/Assets/Scripts/Combat/Weapon/Sword.cs
@@ -1,8 +1,36 @@
-﻿public class Sword : Weapon
+﻿using UnityEngine;
+
+public class Sword : Weapon
 {
-    private const int baseDamage = 1;
+    private const int maxDamage = 3;
+    private const int minDamage = 1;
+
+    private int chanceToCrit = 2;
+    private int critMultiplier = 2;
 
     public Sword() : base(baseDamage)
     {
+    }
+
+    private int CalculateDamage()
+    {
+        int damage = Random.Range(minDamage, maxDamage);
+        if (RollCriticalChance())
+        {
+            damage *= critMultiplier;
+        }
+        return damage;
+    }
+
+    private bool RollCriticalChance()
+    {
+        int percentage = Random.Range(0, 10);
+        if (percentage < chanceToCrit)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
