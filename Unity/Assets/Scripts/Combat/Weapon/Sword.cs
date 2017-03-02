0fe5d09d0acd3cbfdff274d14a906afa1550f4d2
DanC <kcc25@kent.ac.uk>
2017-03-02 17:05:59 +0000

    Made weapons have variable damage with a chance to crit

--- Unity/Assets/Scripts/Combat/Weapon/Sword.cs
+++ Unity/Assets/Scripts/Combat/Weapon/Sword.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Sword : Weapon2
+public class Sword : Weapon
 {
     private const int maxDamage = 3;
     private const int minDamage = 1;
@@ -19,7 +19,7 @@ public class Sword : Weapon2
 
     private int CalculateDamage()
     {
-        int damage = Random.Range(minDamage, maxDamage);
+        int damage = Random.Range(minDamage, maxDamage + 1);
         if (RollCriticalChance())
         {
             damage *= critMultiplier;
@@ -37,71 +37,3 @@ public class Sword : Weapon2
         return false;
     }
 }
-
-public abstract class Weapon2 : MonoBehaviour
-{
-    private bool isColliding;
-    private bool blocked;
-
-    public Weapon2()
-    {
-        isColliding = false;
-        blocked = false;
-    }
-
-    //TODO: set to false once AI and VR player has implemented the ability to toggle weapon hit boxes
-    // Kept to true to for compatability issues until earlier statement is resolved
-    void Start()
-    {
-        setWeaponIsActive(true);
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (isColliding) return;
-        isColliding = true;
-
-        switch (other.collider.gameObject.tag)
-        {
-            case "Weapon":
-                blocked = true;
-                break;
-            case "Shield":
-                blocked = true;
-                break;
-            case "Monster":
-                ApplyDamageToMonster(other.collider);
-                break;
-            case "Player":
-                ApplyDamageToMonster(other.collider);
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void ApplyDamageToMonster(Collider other)
-    {
-        if (blocked)
-        {
-            blocked = false;
-        }
-        else
-        {
-            other.gameObject.GetComponent<IDamageable>().ApplyDamage(GetDamage());
-        }
-    }
-
-    public void setWeaponIsActive(bool isActive)
-    {
-        GetComponent<BoxCollider>().enabled = isActive;
-    }
-
-    void FixedUpdate()
-    {
-        isColliding = false;
-    }
-
-   public abstract int GetDamage();
-}
-
