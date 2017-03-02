041c9dda6cfb65df18a913d2a0e6083e687130cb
Japeth <Japeth.Gurr@hotmail.co.uk>
2017-03-02 20:47:12 +0000

    Prevented player from clipping themselves into geometry via locomotion

---
+++ Unity/Assets/Scripts/VR/CollisionTester.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class CollisionTester : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On Trigger Enter: " + other);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("On Trigger Stay: " + other);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("On Trigger Exit: " + other);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("On Collision Enter: " + other);
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        Debug.Log("On Collision Stay: " + other);
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        Debug.Log("On Collision Enter: " + other);
+    }
+}
