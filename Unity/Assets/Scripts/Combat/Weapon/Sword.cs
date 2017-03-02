c3ff379ba295a881d02d6bfffd4514947e4f4d86
DanC <kcc25@kent.ac.uk>
2017-03-02 13:44:34 +0000

    WIP making a temp copy of Weapon in Sword to modify how weapon is getting damage from its child classes

--- Unity/Assets/Scripts/Combat/Weapon/Sword.cs
+++ Unity/Assets/Scripts/Combat/Weapon/Sword.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Sword : Weapon
+public class Sword : Weapon2
 {
     private const int maxDamage = 3;
     private const int minDamage = 1;
@@ -8,8 +8,13 @@ public class Sword : Weapon
     private int chanceToCrit = 2;
     private int critMultiplier = 2;
 
-    public Sword() : base(baseDamage)
+    public Sword() : base()
     {
+    }
+
+    public override int GetDamage()
+    {
+        return CalculateDamage();
     }
 
     private int CalculateDamage()
@@ -32,5 +37,72 @@ public class Sword : Weapon
         else {
             return false;
         }
+    }
+}
+
+public abstract class Weapon2 : MonoBehaviour
+{
+    private bool isColliding;
+    private bool blocked;
+
+    public Weapon2()
+    {
+        isColliding = false;
+        blocked = false;
+    }
+
+    //TODO: set to false once AI and VR player has implemented the ability to toggle weapon hit boxes
+    // Kept to true to for compatability issues until earlier statement is resolved
+    void Start()
+    {
+        setWeaponIsActive(true);
+    }
+
+    public abstract int GetDamage();
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (isColliding) return;
+        isColliding = true;
+
+        switch (other.collider.gameObject.tag)
+        {
+            case "Weapon":
+                blocked = true;
+                break;
+            case "Shield":
+                blocked = true;
+                break;
+            case "Monster":
+                ApplyDamageToMonster(other.collider);
+                break;
+            case "Player":
+                ApplyDamageToMonster(other.collider);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ApplyDamageToMonster(Collider other)
+    {
+        if (blocked)
+        {
+            blocked = false;
+        }
+        else
+        {
+            other.gameObject.GetComponent<IDamageable>().ApplyDamage(GetDamage());
+        }
+    }
+
+    public void setWeaponIsActive(bool isActive)
+    {
+        GetComponent<BoxCollider>().enabled = isActive;
+    }
+
+    void FixedUpdate()
+    {
+        isColliding = false;
     }
 }
