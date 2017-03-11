4667a639e7b4ce168a1a51e1d10cb05ccb420fb5
Jamie Redding <jamieredding2694@gmail.com>
2017-03-11 16:04:45 +0000

    Randomly spawning torches in rooms
    Also includes preliminary support for spawning new room shapes.

---
+++ Unity/Assets/Scripts/World/Torch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace World
+{
+    public class Torch : MonoBehaviour
+    {
+        private GameObject offTorch;
+        private GameObject onTorch;
+
+        private readonly float SPAWN_CHANCE = 0.4f;
+
+        void Start()
+        {
+
+            GetSubTorchModels();
+            if (Random.value < SPAWN_CHANCE) {
+                onTorch.SetActive(true);
+            }
+            else {
+                offTorch.SetActive(true);
+            }
+        }
+
+        private void GetSubTorchModels()
+        {
+            foreach (Transform child in transform) {
+                if (child.name.Equals("torch_1_empty")) {
+                    offTorch = child.gameObject;
+                } else if (child.name.Equals("torch_1")) {
+                    onTorch = child.gameObject;
+                }
+            }
+            if (offTorch == null || onTorch == null) {
+                throw new ArgumentException("This parent torch has doesn't contain a torch and empty torch.");
+            }
+        }
+    }
+}
