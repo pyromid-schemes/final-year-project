6382c9f4c1f30c5e3cbcd7387abf61d4e87097ae
Japeth <Japeth.Gurr@hotmail.co.uk>
2017-03-14 11:46:54 +0000

    Updated haptics, removed old lance (hitbox issues), tweaked item map

--- Unity/Assets/Scripts/Combat/Weapon/Equippable.cs
+++ Unity/Assets/Scripts/Combat/Weapon/Equippable.cs
@@ -5,22 +5,27 @@ public class Equippable : MonoBehaviour {
 
     SteamVR_Controller.Device HoldingHand = null;
 
+    private float PulseTimeRemaningS = 0;
+
+    void FixedUpdate()
+    {
+        if(PulseTimeRemaningS > 0)
+        {
+            PulseTimeRemaningS -= Time.deltaTime;
+            HoldingHand.TriggerHapticPulse(500);
+        }
+        else
+        {
+            PulseTimeRemaningS = 0;
+        }
+    }
+
     void OnCollisionEnter(Collision col)
     {
         CheckIfPlayerCollision(col.collider);
     }
 
-    void OnCollisionExit(Collision col)
-    {
-        CheckIfPlayerCollision(col.collider);
-    }
-
     void OnTriggerEnter(Collider other)
-    {
-        CheckIfPlayerCollision(other);
-    }
-
-    void OnTriggerExit(Collider other)
     {
         CheckIfPlayerCollision(other);
     }
@@ -31,7 +36,28 @@ public class Equippable : MonoBehaviour {
         {
             if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
             {
-                HoldingHand.TriggerHapticPulse(1000);
+                PulseTimeRemaningS = 0.1f;
+                switch (other.gameObject.tag)
+                {
+                    case "Monster":
+                        break;
+                    case "Wall":
+                        break;
+                    case "Floor":
+                        break;
+                }
+            }
+            else
+            {
+                switch (other.gameObject.tag)
+                {
+                    case "Weapon":
+                        PulseTimeRemaningS = 0.1f;
+                        break;
+                    case "Shield":
+                        PulseTimeRemaningS = 0.1f;
+                        break;
+                }
             }
         }
     }
--- Unity/Assets/Scripts/VR/ItemMap.cs
+++ Unity/Assets/Scripts/VR/ItemMap.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 public class ItemMap : MonoBehaviour {
     public GameObject SwordPrefab;
     public GameObject ShieldPrefab;
-    public GameObject DaggerPrefab;
+    //public GameObject DaggerPrefab;
     public GameObject LancePrefab;
-    public GameObject MedievalSwordPrefab;
+    //public GameObject MedievalSwordPrefab;
 
     private Dictionary<string, Transform> ItemTransforms;
 
@@ -16,9 +16,9 @@ public class ItemMap : MonoBehaviour {
         ItemTransforms = new Dictionary<string, Transform>();
         ItemTransforms.Add(SwordPrefab.name, SwordPrefab.transform);
         ItemTransforms.Add(ShieldPrefab.name, ShieldPrefab.transform);
-        ItemTransforms.Add(DaggerPrefab.name, DaggerPrefab.transform);
+        //ItemTransforms.Add(DaggerPrefab.name, DaggerPrefab.transform);
         ItemTransforms.Add(LancePrefab.name, LancePrefab.transform);
-        ItemTransforms.Add(MedievalSwordPrefab.name, MedievalSwordPrefab.transform);
+        //ItemTransforms.Add(MedievalSwordPrefab.name, MedievalSwordPrefab.transform);
     }
 
     public Transform GetDefaultTransform(string itemName)
