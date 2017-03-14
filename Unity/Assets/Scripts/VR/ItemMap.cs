9779e9e8dc7e03b219cc1c15bacd8f332f3ba9c5
Japeth <Japeth.Gurr@hotmail.co.uk>
2017-03-14 12:09:09 +0000

    I forgot to take the Lance out of my a- the ItemMap

--- Unity/Assets/Scripts/VR/ItemMap.cs
+++ Unity/Assets/Scripts/VR/ItemMap.cs
@@ -6,7 +6,6 @@ public class ItemMap : MonoBehaviour {
     public GameObject SwordPrefab;
     public GameObject ShieldPrefab;
     //public GameObject DaggerPrefab;
-    public GameObject LancePrefab;
     //public GameObject MedievalSwordPrefab;
 
     private Dictionary<string, Transform> ItemTransforms;
@@ -17,7 +16,6 @@ public class ItemMap : MonoBehaviour {
         ItemTransforms.Add(SwordPrefab.name, SwordPrefab.transform);
         ItemTransforms.Add(ShieldPrefab.name, ShieldPrefab.transform);
         //ItemTransforms.Add(DaggerPrefab.name, DaggerPrefab.transform);
-        ItemTransforms.Add(LancePrefab.name, LancePrefab.transform);
         //ItemTransforms.Add(MedievalSwordPrefab.name, MedievalSwordPrefab.transform);
     }
 
