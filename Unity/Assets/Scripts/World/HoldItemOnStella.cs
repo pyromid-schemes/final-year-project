e13110aabf50ac6f2bac63aa941fd7082a8850bb
Japeth <Japeth.Gurr@hotmail.co.uk>
2017-03-15 11:23:48 +0000

    Added new player starting room

---
+++ Unity/Assets/Scripts/World/HoldItemOnStella.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldItemOnStella : MonoBehaviour {
+	
+    void Awake()
+    {
+        FreeChildren();
+    }
+
+    void FixedUpdate()
+    {
+        FloatItemAbove();
+    }
+
+    void FloatItemAbove()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gameObject.transform.position, Vector3.up, out hit, maxDistance: 1.2f))
+        {
+            if (hit.rigidbody != null && !hit.rigidbody.isKinematic)
+            {
+                hit.rigidbody.velocity = Vector3.zero;
+                hit.rigidbody.angularVelocity = Vector3.zero;
+                hit.rigidbody.Sleep();
+            }
+        }
+    }
+
+    void FreeChildren()
+    {
+        foreach(Transform child in gameObject.transform)
+        {
+            child.parent = null;
+        }
+    }
+}
